ffa5e5619ae9313b44108f08aa3e909233e82e53
Bordercollie15 <yutongminervacao@gmail.com>
2022-06-30 09:12:38 -0500

    Working Website

--- src/containers/TodoList/index.js
+++ src/containers/TodoList/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { TodoItem } from './components/TodoItem'
+
+const todos = ["do homework", "clean room", "practice React"];
+
 
 const TodoList = () => (
-  {/* TODO */}
+  <ul>
+    {todos.map((item, idx) => <TodoItem item={item} />)}
+  </ul>
 )
+
+export default TodoList;
